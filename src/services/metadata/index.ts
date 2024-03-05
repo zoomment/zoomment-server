020044ff21dd71e0a3e89dd91671bf1eefb24e6f
tigran <tigran.contact@gmail.com>
2024-03-06 01:48:56 +0400

    bug fix

--- src/services/metadata/index.ts
+++ src/services/metadata/index.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 export async function fetchSiteToken(url: string) {
   try {
-    const html = await axios.get<string, string>(url, { responseType: 'text' });
-    const $ = cheerio.load(html);
+    const response = await axios.get(url, { responseType: 'text' });
+    const $ = cheerio.load(response.data);
 
     let token: string | undefined;
     $('meta[name^="zoomment"]').each((index, element) => {
