85828aaf2bdebe0c6e9037ec335e3418aef7b042
tigran <tigran.simonyan@hostaways.com>
2025-12-24 02:12:34 +0400

    change validaiton

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,8 @@ for (const envVar of requiredEnvVars) {
   }
 }
 
-if (process.env.JWT_SECRET && process.env.JWT_SECRET.length < 32) {
-  console.error('JWT_SECRET must be at least 32 characters');
+if (process.env.JWT_SECRET && process.env.JWT_SECRET.length < 15) {
+  console.error('JWT_SECRET must be at least 15 characters');
   process.exit(1);
 }
 
